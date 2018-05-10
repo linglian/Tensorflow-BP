3607316a4cfb229af9dc80ee1566376a4cce84e8
Elle-Peng <374237925@qq.com>
2018-05-10 23:28:08 +0800

    完成制作lable和随机化train\test列表

--- tensorflow-code/make_label.py
+++ tensorflow-code/make_label.py
@@ -1,6 +1,14 @@
 from tools import create_label_list
 
-file = open('list.txt', 'w')
+# 将要存入的文件
+list_file_path = 'list.txt'
+file = None
+
+"""将所需文件按照label格式存入到文件中
+
+Raises:
+    ValueError -- file_path为空时发出
+"""
 
 def make_label(file_path):
     if file_path is None:
@@ -10,14 +18,25 @@ def make_label(file_path):
     file.write('{} {}\n'.format(file_path, id_val))
 
 if __name__ == '__main__':
+    
     import sys
     import getopt
-    opts, args = getopt.getopt(sys.argv[1:], 'f:')
+
+    opts, args = getopt.getopt(sys.argv[1:], 'f:l:')
+
     path = None
+    
     for op, value in opts:
         if op == '-f':
            path = value 
+        elif op == '-l':
+            list_file_path = value
+            
     if path is None:
         raise ValueError('Must Enter Path Use -f', path)
         
+    file = open(list_file_path, 'w')
+
     create_label_list.traverse_floder(path, make_label, 'py')
+    
+    file.close()
---
+++ tensorflow-code/make_train_test_list.py
@@ -0,0 +1,54 @@
+#coding=utf-8
+
+import random
+
+train_file_path = 'train_list.txt'
+test_file_path = 'test_list.txt'
+list_file_path = 'list.txt'
+
+train_radio = 0.95
+
+if __name__ == '__main__':
+    
+    import sys
+    import getopt
+
+    opts, args = getopt.getopt(sys.argv[1:], 'r:t:l:b:')
+
+    for op, value in opts:
+        if op == '-r':
+            train_file_path = value
+        elif op == '-t':
+            test_file_path = value
+        elif op == '-l':
+            list_file = value
+        elif op == '-b':
+            train_radio = float(value)
+    
+    train_file = open(train_file_path, 'w')
+    test_file = open(test_file_path, 'w')
+    list_file = open(list_file_path, 'r')
+
+    files_list = list_file.readlines()
+
+    list_file.close()
+
+    # 将数据打乱
+    random.shuffle(files_list)
+
+    # 获取训练数据数量
+    train_size = int(len(files_list) * train_radio)
+
+    # 写入训练数据
+    for file in files_list[:train_size]:
+        train_file.write(file)
+    
+    train_file.close()
+
+    # 写入验证数据
+    for file in files_list[train_size:]:
+        test_file.write(file)
+    
+    test_file.close()
+        
+
