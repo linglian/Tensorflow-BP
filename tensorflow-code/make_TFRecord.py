866f38c23b69c2bc4276b33f935f661b9edbc755
Elle-Peng <374237925@qq.com>
2018-05-11 22:16:10 +0800

    完成制作.tfrecord

--- tensorflow-code/make_TFRecord.py
+++ tensorflow-code/make_TFRecord.py
@@ -1,5 +1,64 @@
 #coding=utf-8
 
-import tensorflow as tf
+from tools import create_label_list
+import sys
 
-tf.train.Example
+models_path = '/Users/lol/DeepLearn/models'
+
+def make_TFRecord(list_file_path, tfrecord_file_path=None):
+    from datasets import dataset_utils
+
+    import tensorflow as tf
+    
+    # 默认情况下保存为列表名_tfrecord.tfrecords
+    if tfrecord_file_path is None:
+        tfrecord_file_path = create_label_list.getFileName(list_file_path).split('.')[0] + '_tfrecord.tfrecords'
+
+    # 图片列表文件
+    list_file = open(list_file_path)
+
+    # 获取每行，并且分隔
+    lines = [line.split() for line in list_file]
+
+    list_file.close()
+
+    tfrecord_writer = tf.python_io.TFRecordWriter(tfrecord_file_path)
+
+    with tf.Graph().as_default():
+        decode_jpeg_data = tf.placeholder(dtype=tf.string)
+        decode_jpeg = tf.image.decode_jpeg(decode_jpeg_data, channels=3)
+        with tf.Session('') as sess:
+            for file_name, label in lines:
+                image_data = tf.gfile.FastGFile(file_name, 'rb').read()
+                image = sess.run(decode_jpeg, feed_dict={decode_jpeg_data: image_data})
+                height, width = image.shape[0], image.shape[1]
+                example = dataset_utils.image_to_tfexample(
+                    image_data, b'jpg', height, width, int(label))
+                tfrecord_writer.write(example.SerializeToString())
+        
+    
+if __name__ == '__main__':
+    import sys
+    import os
+    import getopt
+
+    opts, args = getopt.getopt(sys.argv[1:], 'f:t:s:')
+
+    list_file_path = 'list.txt'
+    tfrecord_file_path = None
+
+    for op, val in opts:
+        if op == '-f':
+            list_file_path = val
+        elif op == '-t':
+            tfrecord_file_path = val
+        elif op == '-s':
+            models_path = val
+        
+    sys.path.insert(0, models_path + '/research/slim/') #把后面的路径插入到系统路径中 idx=0
+    print('%s is Loaded' % (models_path + '/research/slim/'))
+    make_TFRecord(list_file_path, tfrecord_file_path)
+
+
+
+
