a7533472b0862a434b5edb0ac93424c0c5d95a1e
Elle-Peng <374237925@qq.com>
2018-05-16 22:13:08 +0800

    sadsad

--- tensorflow-code/resnet_v2/Server.py
+++ tensorflow-code/resnet_v2/Server.py
@@ -41,7 +41,7 @@ def rename_filename(old_file_name):
   return new_name
 
 def inference(file_name):
-  image_date = tf.gfile.FastGFile(filename, 'rb').read()
+  image_date = tf.gfile.FastGFile(file_name, 'rb').read()
   predictions, top_k, top_names = run_inference_on_image(image_date, model=FLAGS.model)
   new_url = '/static/%s' % os.path.basename(file_name)
   image_tag = '<img src="%s"></img><p>'
